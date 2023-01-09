9bc1e6cd986191c10dcf1d10af80702fd209531a
mertK <mert.katircioglu@gmail.com>
2023-01-09 18:52:14 +0300

    -en & tr language packs are added,

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:bitky/globals/globals.dart';
+import 'package:bitky/l10n/app_localizations.dart';
+import 'package:bitky/l10n/app_localizations_tr.dart';
 import 'package:bitky/restart_app.dart';
 import 'package:bitky/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'l10n/l10.dart';
 import 'locator.dart';
 
 class MyHttpOverrides extends HttpOverrides{
@@ -34,9 +38,11 @@ class MyApp extends StatelessWidget with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Bitky Guide',
-
+      supportedLocales: L10n.all,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
